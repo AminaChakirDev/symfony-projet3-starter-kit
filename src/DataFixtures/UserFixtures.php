1741cf4a16ba9ee63a3892c6b01f9b5fb5b883e6
Altyrion69 <altyrion@hotmail.fr>
2024-01-31 23:21:32 +0100

    fixture+user with make+index de artwork

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -9,26 +9,29 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
-    private UserPasswordHasherInterface $passwordHasher;
+  private UserPasswordHasherInterface $passwordHasher;
 
-    public function __construct(UserPasswordHasherInterface $passwordHasher)
-    {
-      $this->passwordHasher = $passwordHasher;
-    }
-
-    public function load(ObjectManager $manager):void
-    {
-      $user = new User();
-      $user->setName('altyrion');
-      $user->setEmail('altyrion@hotmail.fr');
-      $user->setPassword($this->passwordHasher->hashPassword($user,'azerty123'));
-      $user->setDescription(
-        'Je suis passionné par l\'art et les innovations technologiques. Toutefois, comme je ne sais ni peindre ni dessiner,
-      l\'intelligence artificielle est devenue le pinceau avec lequel je peux illustrer l\'univers que j\'ai toujours rêvé de concevoir,
-      me fournissant ainsi une toile numérique pour concrétiser ma vision.');
-      $user->setRoles(['ROLE_USER']);
-
-      $manager->persist($user);
-      $manager->flush();
-    }
+  public function __construct(UserPasswordHasherInterface $passwordHasher)
+  {
+    $this->passwordHasher = $passwordHasher;
   }
+
+  public function load(ObjectManager $manager): void
+  {
+        $user = new User();
+        $user->setName('altyrion');
+        $user->setEmail('altyrion@hotmail.fr');
+        $user->setPassword($this->passwordHasher->hashPassword($user, 'azerty123'));
+        $user->setDescription(
+      'Je suis passionné par l\'art et les innovations technologiques. 
+              Toutefois, comme je ne sais ni peindre ni dessiner,
+            l\'intelligence artificielle est devenue le pinceau avec lequel je peux illustrer 
+            l\'univers que j\'ai toujours rêvé de concevoir,
+            me fournissant ainsi une toile numérique pour concrétiser ma vision.'
+    );
+    $user->setRoles(['ROLE_USER']);
+
+    $manager->persist($user);
+    $manager->flush();
+  }
+}
